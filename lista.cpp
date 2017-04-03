ebd6d784a4117c34e293fdff447dde6ceba02190
Przemcom <Przemcom@gmail.com>
2017-04-03 12:35:47 +0200

    Add files via upload
    lista.cpp 3.04.17 przed zajeciami

---
+++ lista.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+class el{
+	public:
+		//konstruktor niepotrzebny
+		
+		int wart;
+		el *nast;
+};
+
+
+class lista{
+	public:
+		el *pierwszy;
+		
+		
+		
+		//kontruktor
+		lista(){
+			cout << "konstruktor bezargumentowy listy: checked"<<endl;
+			pierwszy = NULL;
+
+			
+		}
+		//destruktor
+		~lista(){
+			cout<< "destruktor bezargumentowy listy: checked" << endl;
+			el *wsk, *tmp;
+			wsk = this->pierwszy;
+			this->pierwszy = NULL;
+			while(wsk!=NULL){
+				tmp = wsk;
+				delete wsk;
+				wsk = tmp->nast;
+			}
+		}
+		
+		friend ostream& operator<<(ostream &s, lista &a){
+			el *wsk;
+			wsk=a.pierwszy;
+			
+				while(wsk!=NULL){
+					s << wsk->wart<<" ";
+					wsk = wsk->nast;
+				}
+				s << endl;
+			return s;
+		}
+		
+		friend istream& operator>>(istream &s, lista &a){
+			
+			el *nowy = new el;
+			nowy->nast = NULL;
+			s >> nowy->wart; 
+			
+		
+			if (a.pierwszy == NULL){
+				a.pierwszy = nowy;
+			}
+			
+			else{
+			el *wsk = a.pierwszy;
+				while (wsk->nast!=NULL){
+					wsk = wsk->nast;
+					
+				}
+			
+			wsk->nast = nowy;
+			
+		}
+		return s;
+	}
+	
+	lista& operator=(lista &b){
+			cout << "Operator '=' pracuje" << endl;
+			
+			el *wsk;
+			
+			wsk = this->pierwszy;
+			
+			while(wsk!=NULL){
+				el *nowy = new el;
+				nowy->nast = NULL;
+				nowy = wsk;
+				wsk = wsk->nast;
+				
+			}
+			
+			return b;
+		}	
+};
+
+
+int main(int argc, char** argv) {
+	
+	lista a,b;
+	
+	cin>>a>>a>>a;
+	cout<<a;
+	cout<<(b=a);
+	//a.~lista();
+	
+	//cout<<a;
+	
+	return 0;
+}//copyright R by Fryderyk Muras & Przemyslaw Michalek 2017
